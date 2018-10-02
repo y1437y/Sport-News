42135413c6eaab2a27e1c06ee524625e6a774d14
Chien Jung Huang <y1437y@gmail.com>
2018-10-02 12:57:02 -0500

    Initial Commit

---
+++ Sport News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sport News
+//
+//  Created by Hayden Kaci on 10/2/18.
+//  Copyright © 2018 Chien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
